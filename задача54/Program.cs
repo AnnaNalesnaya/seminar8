1f93787351993c6420311ba827f523567fb0eb15
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-10-14 01:00:52 +0300

    задача 54

---
+++ задача54/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int [,] FillArray (int numLine, int numColumns, int maxRand = 20, int minRand = 0)
+ {
+    int[,] matrix = new int[numLine, numColumns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(minRand, maxRand);
+        }
+    }
+    return  matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] BubbleSortLines(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            bool flagSort = true;
+            for (int j = 0; j < matrix.GetLength(1) - 1 - k; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                    flagSort = false;
+                }
+            }
+            if (flagSort) { break; }
+        }
+    }
+    return matrix;
+}
+int[,] array = FillArray(3, 5);
+PrintArray(array);
+System.Console.WriteLine();
+int[,] answer = BubbleSortLines(array);
+PrintArray(answer);
